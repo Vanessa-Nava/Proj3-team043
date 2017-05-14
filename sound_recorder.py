65ad0b6fc0abf1c91cc2dc44b5bea516691c8379
Genesis Valdez <gevaldez@csumb.edu>
2017-05-14 09:59:18 +0000

    changes in this file

--- sound_recorder.py
+++ sound_recorder.py
@@ -1,68 +1,80 @@
-import pyaudio
-import audioop 
 import wave
-import collections
-import numpy as np
+import numpy as np 
 import soundfile as sf
-#import sys
-import SpeechRecognition as sr
+from scipy.fftpack import fft
+import pymatlab.matlab 
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
-#CHUNK=  512 # by using the ASCII table for the wave sound file as bytes
-#wave_file= wave.open("iCry.wav")
-r = sr.Recognizer()
-with sr.Microphone(device_index = 2, sample_rate = 44100, chunk_size = 512) as source:
-    print("Say something!")
-    audio = r.listen(source)
+#1,000 to 5,000 Hz average is 3,500Hz
+
+wave_file= wave.open("iCry (1).wav")
+rate, sound= sf.read("iCry (1).wav")
+sf.write('new_file.ogg',rate, sound)
+
+print(sound)
+print (rate)
+
+framerate= wave_file.getframerate()
+frames= wave_file.getnchannels()
+width= wave_file.getsampwidth()
+
+print("sampling rate: ", framerate, "Hz")
+print("length: ", frames, "samples")
+print("sample width: ", width, "bytes")
+
+sound = 48000 # samples 
+t=np.linspace(0, len(rate)/sound, len(sound), dytype=int)
+plt.plot(t,sound)
+
+'''data=[sound, rate]
+t=np.zeros((rate, sound), dytpe=int)
+w = np.fft.fft(np.sin(t))
+ind_max = np.argmax(np.abs(w))
+freq = data[ind_max]
+freq_in_hertz = abs(freq * Fs)
+t=np.linspace(0, len(y)/Fs, len(y)) #linspace is the fuction that will create the time vector
+plt.plot(t,y)'''
 
 
-'''swidth= wave_file.getsampwidth()
-RATE= wave_file.getframerate() 
-CHANNELS= wave_file.getnchannels()
-Record_Sec= 17
+#y = np.zeros(Fs)
+#x1 = np.linspace(0, 10, Fs, endpoint=True)
+#x2 = np.linspace(0, 10, Fs, endpoint=False)
+#plt.plot(x1, y, 'o')
+
+#plt.plot(x2, y + 0.5, 'o')
+
+#plt.ylim([-0.5, 1])
+#(-0.5, 1)
+#plt.show()
+# this code works 
+
+'''Fs= 4800
+T=1/Fs
+
+x = linspace(0.0, Fs*T, Fs)
+fftLength= length(data)
+xdft= fft(data, fftLength)
+maxAmp= max(a)
+t = np.append(rate, sound)
+sp = np.fft.fft(np.sin(t))
+freq = np.fft.fftfreq(t.shape[-1])
+plt.plot(freq, sp.real, freq, sp.imag)
+
+plt.show()'''
+
+'''w = np.fft.fft(np.sin(data)
+freqs = np.fft.fftfreq(n, d=1./rate)
+
+print(freqs.min(), freqs.max())'''
 
 
-#using a blackman window to go through array
-window= np.blackman(CHUNK)
-sys.exit("EXIT......")
-audio= pyaudio.PyAudio()
+# Find the peak in the coefficients
+    #find_max= index of first peaks in w
 
-stream= audio.open(format= audio.get_format_from_width(wave_file.getsampwidth()),
-                   CHANNELS= wave_file.getnchannels(), RATE=wave_file.getframerate(), output= True)
+'''ind_max = np.argmax(np.abs(w))
+freq = freqs[ind_max]
+freq_in_hertz = abs(freq * framerate)'''
 
-sf= collections.deque(maxlen= RATE/CHUNK)
-#find the frequency of each chunk 
-flag= True 
-print( "Listening... ")
-for i in range(0, int(RATE/CHUNK* Record_Sec)):
-    data=stream.read(CHUNK)
-    sf.append(abs(audioop.avg(data,4)))
-    
-print(sum(sf)/RATE/CHUNK*Record_Sec)
 
-stream.stop_stream()
-stream.close()
-audio.terminate()
-
-def listen():
-    audio=pyaudio.PyAudio()
-    audio.open(format= audio.get_format_from_width(wave_file.getsampwidth()),
-                   CHANNELS= wave_file.getnchannels(), RATE=wave_file.getframerate(), input= True)
-
-    sf=collections.deque(maxlen= RATE/CHUNK)
-    flag=True;
-    print("Listening... ")
-    
-    while(True):
-        data= stream.read(CHUNK)
-        sf.append(abs(audioop.avg(data,4)))
-        if(flag==True):
-            if (sum(sf)/(RATE/CHUNK*Record_Sec) < 5500000):
-                print("off")
-                flag=False 
-                
-        else:
-            if(sum(sf)/(RATE/CHUNK*Record_Sec) > 5500000):
-                print("on")
-                flag=True'''
-                
-   
